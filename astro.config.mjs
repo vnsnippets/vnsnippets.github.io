81312a80fe0e6ffd17192fbfef1a43a3e71cc6e3
Vidush H. Namah <vidush.namah@orion-next.com>
2026-07-15 13:34:55 +0400

    Migrate MD to MDX with image loading effects (#20)
    #### Added
    - Image container sizes (width, height) calculated during site
    generation
      - Shimmer effect while image loads
    
    #### Changed
    - Migrated all blog articles from `.md` to `.mdx`
    - Integrated new image loading (shimmer) effect into blog article images
      - Use `astro-expressive-code` for code blocks
    - Changed site icons
    
    #### Fixed
    None
    
    ### Removed
    - Old `.md` articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,26 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vnsnippets.github.io",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
+  integrations: [
+    expressiveCode({
+      // Highly customizable: e.g., dual light/dark themes
+      themes: [ "catppuccin-macchiato", "catppuccin-latte" ], 
+    }),
+    mdx()
+  ],
   markdown: {
-    shikiConfig: {
-      // Choose any built-in theme (e.g., "dracula", "nord", "css-variables")
-      theme: "catppuccin-macchiato", 
-    },
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: "catppuccin-macchiato" }
   },
+  image: {
+    domains: ["github.com", "raw.githubusercontent.com"]
+  }
 });
