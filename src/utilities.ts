55fca8271bf8de751227db6e4e950b36c2b5c8e8
Vidush H. Namah <vidush.namah@orion-next.com>
2026-07-19 14:31:03 +0400

    Show article author and date

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,8 +5,6 @@ export const SiteSettings = Site;
 
 export const GetPageProperties = <T extends Record<string, any>>(path: string) => {
     const pages = SiteSettings.Pages as unknown as Record<string, { Metadata: PageMetadata } & T>;
-    
-    // Normalize path: replace trailing slash unless it's the root "/"
     const normalizedPath = path === "/" ? path : path.replace(/\/$/, "");
     return pages[normalizedPath];
 };
@@ -16,7 +14,15 @@ export const FilterDraftsOnProduction = ({ data }: { data: { Draft?: boolean } }
 };
 
 export const ToShortDate = (e: Date) => {
-    return e.toISOString().split('T')[0]
+    return e.toISOString().split("T")[0]
+}
+
+export const ToDescriptiveDate = (e: Date) => {
+    return new Intl.DateTimeFormat("en-US", {
+        month: "long",
+        day: "2-digit",
+        year: "numeric"
+    }).format(e);
 }
 
 export const Slugify = (e: string) => e.trim().toLowerCase().replace(/\s+/g, "-");
